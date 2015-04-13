ba4a1cc670899ce8fc4653b54ad226d25f0486ba
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-13 12:53:49 +0100

    Solved the fizz, wrote tests for the buzz

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -3,10 +3,14 @@ describe("FizzBuzz",function() {
   it("should display 'fizz' when a num is divisible by three", function() {
     fizzbuzz = new FizzBuzz();
 
-    expect(fizzbuzz.result(3)).toBe(true);
+    expect(fizzbuzz.result(3)).toBe("fizz");
+    expect(fizzbuzz.result(6)).toBe("fizz");
+    expect(fizzbuzz.result(2)).not.toBe("fizz");
   });
-
-
-
+  it("should display 'buzz' whena num is divisible by five", function() {
+    expect(fizzbuzz.result(5)).toBe("buzz");
+    expect(fizzbuzz.result(25)).toBe("buzz");
+    expect(fizzbuzz.result(19)).not.toBe("buzz");
+  });
 });
 
