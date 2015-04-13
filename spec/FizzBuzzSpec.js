4f6e4749a5e825871b2249884bdcddeca8b8d01d
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-13 14:24:38 +0100

    displays buzz when a num is divisible by 5

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,16 +1,25 @@
 describe("FizzBuzz",function() {
 
+  var fizzbuzz = new FizzBuzz();
+
   it("should display 'fizz' when a num is divisible by three", function() {
-    fizzbuzz = new FizzBuzz();
 
     expect(fizzbuzz.result(3)).toBe("fizz");
     expect(fizzbuzz.result(6)).toBe("fizz");
     expect(fizzbuzz.result(2)).not.toBe("fizz");
   });
-  it("should display 'buzz' whena num is divisible by five", function() {
+  it("should display 'buzz' when a num is divisible by five", function() {
+
     expect(fizzbuzz.result(5)).toBe("buzz");
     expect(fizzbuzz.result(25)).toBe("buzz");
     expect(fizzbuzz.result(19)).not.toBe("buzz");
+  });
+
+  it("should display a num when not divisible by 3, 5 or 15", function() {
+    expect(fizzbuzz.result(2)).toBe(2)
+    expect(fizzbuzz.result(5)).not.toBe(5)
+    expect(fizzbuzz.result(3)).not.toBe(3)
+    expect(fizzbuzz.result(15)).not.toBe(15)
   });
 });
 
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -3,6 +3,8 @@ function FizzBuzz() {
 
 FizzBuzz.prototype.result = function(num) {
   if(num % 3 === 0) {
-    return "fizz"
+    return "fizz";
+  } else if (num % 5 === 0) {
+    return "buzz";
   };
 };
