00c8ec2e6e87cdd41c691debf010c51fdf009dd0
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-13 16:00:18 +0100

    created a simple html page which displays fizzbuzz upto 10,000

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -2,11 +2,13 @@ function FizzBuzz() {
 };
 
 FizzBuzz.prototype.result = function(num) {
-  if(num % 3 === 0) {
-    return "fizz";
+  if(num % 15 === 0) {
+    return "fizzbuzz";
   } else if (num % 5 === 0) {
     return "buzz";
-  } else if (num % 15 !== 0) {
-    return num
+  } else if (num % 3 === 0) {
+    return "fizz";
+  } else {
+    return num;
   };
 };
